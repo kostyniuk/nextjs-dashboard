8d13f14222d4841c5191edb32135a6c0f2dbb0b9
Alex Kostyniuk <kostyniuk7@gmail.com>
2025-09-27 23:59:12 +0200

    make seed page is also reachable

--- auth.config.ts
+++ auth.config.ts
@@ -8,9 +8,14 @@ export const authConfig = {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      const isOnSeed = nextUrl.pathname.startsWith('/seed');
+      
       if (isOnDashboard) {
+        console.log(nextUrl);
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
+      } else if (isOnSeed) {
+        return true; // Allow access to seed route without authentication
       } else if (isLoggedIn) {
         return Response.redirect(new URL('/dashboard', nextUrl));
       }
